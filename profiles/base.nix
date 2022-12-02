6a85e84a355ec370b9b57a654bcddcdecd85d8d8
Nikhil Jha <nikhiljha@users.noreply.github.com>
2022-12-02 10:46:29 -0800

    add: fydai to emergency acl

--- profiles/base.nix
+++ profiles/base.nix
@@ -19,6 +19,7 @@
     extraGroups = [ "wheel" ];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGiiq/rSfG+bKmqKZfCSl1z2r7rc3Wt/Paya/JYmjdSO strudel@nikhiljha.com"
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDrkdSorbUk6DtvdsKtS50CS7icElFQIC8rgjI4aOXCzcazTWRJB82x1dwDNIihxZB9KRQJ2939mUO3UhDfrB1JFlFvDtbH3FsZBxjn7eHZg3Z4wE8UjRwf1j/DBACoO3vBjRkIWE4r1auTMvFbA+jMYcfEv2qWX1xyGj6e2NmhSftfMQeEfucgtkxeq/sPMLXNAL53z9YvI0g7JLGTM0a59h6REccWwifgnSGYQRbKeO13+RCvQWODufjJ6xq4A6lruh5Ajh87MYFzjkHcEqw2EF6c9eT9bX51yOTPBoRNvA+RU0ZjKYtenD2WRU6Q3Px3uKIuFIi8lCuie7flsWsr fydai@ocf.berkeley.edu"
     ];
   };
   
