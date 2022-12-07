ca5d07b16dc23800490186ae73a98dad5b7ded94
Nikhil Jha <nikhiljha@users.noreply.github.com>
2022-12-07 14:11:22 -0800

    add teleport

--- profiles/base.nix
+++ profiles/base.nix
@@ -19,7 +19,8 @@
     extraGroups = [ "wheel" ];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGiiq/rSfG+bKmqKZfCSl1z2r7rc3Wt/Paya/JYmjdSO strudel@nikhiljha.com"
-      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDrkdSorbUk6DtvdsKtS50CS7icElFQIC8rgjI4aOXCzcazTWRJB82x1dwDNIihxZB9KRQJ2939mUO3UhDfrB1JFlFvDtbH3FsZBxjn7eHZg3Z4wE8UjRwf1j/DBACoO3vBjRkIWE4r1auTMvFbA+jMYcfEv2qWX1xyGj6e2NmhSftfMQeEfucgtkxeq/sPMLXNAL53z9YvI0g7JLGTM0a59h6REccWwifgnSGYQRbKeO13+RCvQWODufjJ6xq4A6lruh5Ajh87MYFzjkHcEqw2EF6c9eT9bX51yOTPBoRNvA+RU0ZjKYtenD2WRU6Q3Px3uKIuFIi8lCuie7flsWsr fydai@ocf.berkeley.edu"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA2yyisYL1t5u4f0FPBFKs0jAr4MWZphCb8beBvu7xSw etw@ocf.berkeley.edu"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFVE2dTNVA/m2VeEq18HTWOhDvFYr33O0OW0ivBKEFJc rjz@ocf.berkeley.edu"
     ];
   };
   
@@ -50,6 +51,19 @@
 
   # TODO: Replace me with teleport SSH!
   services.openssh.enable = true;
+
+  # Teleport
+  services.teleport.enable = true;
+  services.teleport.settings = {
+    teleport = {
+      nodename = config.networking.hostName;
+    };
+    ssh_service = {
+      enabled = true;
+    };
+    proxy_service.enabled = false;
+    auth_service.enabled = false;
+  };
 
   networking.firewall.enable = false;
   networking.firewall.allowedTCPPorts = [ 22 ];
