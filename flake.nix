a789a307a1d416f788ee1d1afb793816fbeab403
Nikhil Jha <nikhiljha@users.noreply.github.com>
2023-12-22 19:26:17 -0800

    feat: update to NixOS 23.11, keeping Kubernetes version constant

--- flake.nix
+++ flake.nix
@@ -2,14 +2,14 @@
   description = "NixOS Configuration for the Open Computing Facility <https://ocf.berkeley.edu>";
 
   inputs = {
-    # Pinned to NixOS 22.11 beta since that's coming out soon anyway
-    nixpkgs.url = github:nixos/nixpkgs/nixos-22.11;
+    # Global NixOS system version
+    nixpkgs.url = "github:NixOS/nixpkgs/23.11";
 
     # Separate nixpkgs pin for Kubernetes (we don't want to accidentally update that)
-    kubePin.url = github:nixos/nixpkgs/4d2b37a84fad1091b9de401eb450aae66f1a741e;
+    kubePin.url = "github:nixos/nixpkgs/4d2b37a84fad1091b9de401eb450aae66f1a741e";
 
     # Some helper methods...
-    flakeUtils.url = github:gytis-ivaskevicius/flake-utils-plus/v1.3.1;
+    flakeUtils.url = "github:gytis-ivaskevicius/flake-utils-plus/v1.4.0";
   };
 
   outputs = { self, flakeUtils, ... }@inputs:
