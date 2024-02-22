d93de4e1e9e0b1d3ff9c9165f59a540e2e8b155a
Oliver Ni <oliver.ni@gmail.com>
2024-02-22 13:56:51 -0800

    rewrite flake config

--- flake.nix
+++ flake.nix
@@ -2,47 +2,48 @@
   description = "NixOS desktop configuration for the Open Computing Facility";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils-plus.url = github:gytis-ivaskevicius/flake-utils-plus/v1.4.0;
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = inputs@{ self, nixpkgs, flake-utils-plus }: flake-utils-plus.lib.mkFlake {
-    inherit self inputs;
-    supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+  outputs = inputs@{ self, nixpkgs, flake-utils }:
+    let
+      # Put modules common to all hosts here.
+      commonModules = [ ./profiles/base.nix ];
 
-    # =============
-    # NixOS systems
-    # =============
+      # Put modules for specific hosts here.
+      hosts = {
+        snowball = [ ./hosts/snowball.nix ./profiles/auth.nix ./profiles/desktop.nix ];
+      };
 
-    hostDefaults.modules = [ ./profiles/base.nix ];
-    hosts.snowball.modules = [ ./hosts/snowball.nix ./profiles/desktop.nix ];
+      # Build comena config
+      colmena = builtins.mapAttrs
+        (host: modules: {
+          imports = commonModules ++ modules;
+          deployment.buildOnTarget = true;
+          deployment.targetUser = "root";
+        })
+        hosts;
 
-    # ==============
-    # colmena config
-    # ==============
+      # Build dev shell config
+      devShells = flake-utils.eachDefaultSystem
+        (system:
+          let pkgs = import nixpkgs { inherit system; };
+          in {
+            default = pkgs.mkShell {
+              packages = [ pkgs.colmena ];
+            };
+          }
+        );
+    in
+    {
+      inherit devShells;
 
-    colmena = {
-      meta = {
-        nixpkgs = import nixpkgs {
-          system = "x86_64-linux";
+      colmena = colmena // {
+        meta = {
+          # This can be overriden by the system-specific configuration
+          nixpkgs = import nixpkgs { system = "x86_64-linux"; };
         };
       };
-
-      snowball = { ... }: {
-        imports = [ ./hosts/snowball.nix ./profiles/base.nix ./profiles/desktop.nix ];
-        deployment.buildOnTarget = true;
-        deployment.targetUser = "root";
-      };
     };
-
-    # ===============
-    # Dev Shell setup
-    # ===============
-
-    outputsBuilder = channels: {
-      devShells.default = channels.nixpkgs.mkShell {
-        packages = [ channels.nixpkgs.colmena ];
-      };
-    };
-  };
 }
