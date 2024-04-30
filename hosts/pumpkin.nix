5c34eb0b8a63e70e3eff3544928abb3089d68e5f
Oliver Ni <oliver.ni@gmail.com>
2024-04-29 22:42:41 -0700

    feat: add fred and pumpkin

---
+++ hosts/pumpkin.nix
@@ -0,0 +1,34 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ../hardware/pumpkin.nix
+  ];
+
+  networking.hostName = "pumpkin";
+
+  fileSystems."/home" = {
+    device = "tmpfs";
+    fsType = "tmpfs";
+    options = [ "size=16G" "mode=755" ];
+  };
+
+  ocf = {
+    auth.enable = true;
+    graphical.enable = true;
+
+    network = {
+      enable = true;
+      interface = "enp8s0";
+      lastOctet = 105;
+    };
+  };
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.11"; # Did you read the comment?
+}
