5603048718c2b52cab6154b1e32acb677f49fd57
Oliver Ni <oliverni@berkeley.edu>
2024-09-07 20:44:29 -0700

    feat: add lexy

---
+++ hosts/lexy.nix
@@ -0,0 +1,32 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ../hardware/ridge-pc.nix
+  ];
+
+  networking.hostName = "lexy";
+
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.nvidia.modesetting.enable = true;
+  hardware.nvidia.open = false;
+
+  ocf = {
+    auth.enable = true;
+    graphical.enable = true;
+    tmpfsHome.enable = true;
+
+    network = {
+      enable = true;
+      lastOctet = 153;
+    };
+  };
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.11"; # Did you read the comment?
+}
